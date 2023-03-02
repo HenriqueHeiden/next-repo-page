2256812f467927efffd18fda232339c985de7c92
HenriqueHeiden <Henrique.heiden@gmail.com>
2023-03-02 01:49:53 -0300

    Initial Link tree nextjs - tailwind

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,50 @@
+import Image from "next/image";
+import data from '../data.json'
+
+function LinkCard({ href, title, image }: { href: string; title: string; image?: string }) {
+  return (
+    <a href={href}
+      className="flex items-center p-1 w-full rounded-md
+     hover:scale-105 transition-all bg-gray-100
+     mb-3 max-w-3xl">
+      <div className="flex text-center w-full">
+        <div className="w-10 h-10">
+        {image && (
+          <Image
+            className="rounded-sm"
+            src={image}
+            alt={title}
+            width={40}
+            height={40}
+          />
+        )}
+        </div>
+        <h2 className="flex justify-center items-center font-semibold
+         w-full text-gray-700 -ml-10">
+          {title}
+        </h2>
+        {/* <p className="text-gray-500">{description}</p> */}
+      </div>
+    </a>
+  );
+}
+
 export default function Home() {
   return (
-    <h1>Hello World</h1>
+    <div className="flex items-center flex-col mx-auto 
+    w-full justify-center mt-16 px-8">
+      <Image
+        className="rounded-full"
+        src={data.avatar}
+        alt={data.name}
+        width={96}
+        height={96}
+      />
+
+      <h1 className="font-bold mt-4 mb-8 text-xl text-white">{data.name}</h1>
+      {data.links.map((link, index) => (
+        <LinkCard key={`${link.href} + ${index}`} {...link} />
+      ))}
+    </div>
   )
 }
- 
