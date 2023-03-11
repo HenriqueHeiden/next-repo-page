ba389bbb5ee88fe858436bfcdfeec5f54c8920dd
HenriqueHeiden <Henrique.heiden@gmail.com>
2023-03-11 18:40:39 -0300

    removendo opcoes do database

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -1,9 +1,6 @@
 import { MongoClient } from 'mongodb';
 const database_url = process?.env?.DATABASE_URL
-const client = new MongoClient(database_url || '', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
+const client = new MongoClient(database_url)
 
 export default async function connect(){
     await client.connect()
